f79558ca2dc15bd208639837853445caaf267648
Knee <lukeedo@icloud.com>
2025-10-03 09:03:50 -0400

    make transparent

--- main.gd
+++ main.gd
@@ -1,5 +1,5 @@
 extends Node3D
 
 func _ready():
-	JavaScriptBridge.eval("msgClient('this is a test');")
+	JavaScriptBridge.eval("msgClient('shell initialized');")
 	get_tree().get_root().set_transparent_background(true)
